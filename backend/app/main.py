2fdde3dbc7109bee4907915817eb5ef99f8ab1db
David Akwuru <daakwuru@gmail.com>
2022-11-20 22:43:48 +0000

    Feat(BAC-23): updated the script (reformatted)

--- backend/app/main.py
+++ backend/app/main.py
@@ -121,7 +121,7 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 
 @app.get("/users/{user_id}", response_model=schema.User, tags=['users'])
-def read_user(user_id: int, db: Session = Depends(get_db), ):
+def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
