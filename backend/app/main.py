521a9ea95b812c29e127d210b31a9fef2f7f66c3
Jacob Peter <87601398+keseven626@users.noreply.github.com>
2022-11-21 22:06:30 +0000

    feat(BAC-10): added endpoint for user update

--- backend/app/main.py
+++ backend/app/main.py
@@ -143,3 +143,7 @@ async def email_verification(request: Request, token: str, db: Session = Depends
         return{
             "status" : "ok",
             "data" : f"Hello {user.first_name}, your account has been successfully verified"}
+
+@app.patch("/user/update/{user_id}", response_model=schema.user_update)
+def update_user(user: schema.user_update, user_id: int, db:Session=_fastapi.Depends(get_session)):
+     return crud.update_user(db=db, user=user, user_id=user_id)
