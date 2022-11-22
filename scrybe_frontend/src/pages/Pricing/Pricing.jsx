63ac6a648dc0ded518a626ff476f3316091c371c
damolabolaji <bolaji.oyindamola@gmail.com>
2022-11-22 14:48:22 +0100

    feat: set out  payment tiers DOM elements in pricing.jsx

--- scrybe_frontend/src/pages/Pricing/Pricing.jsx
+++ scrybe_frontend/src/pages/Pricing/Pricing.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./pricing.module.css";
 import heroImage from "./assets/heroimage.svg";
+import startUpIcon from "./assets/star.svg";
+import growingIcon from "./assets/auto_graph.svg";
+import enterpriseIcon from "./assets/corporate_fare.svg";
+import checkIcon from "./assets/check.svg";
+import currency from "./assets/naira.svg";
 
 function Pricing() {
   return (
-    <>
+    <div className={styles.pricing}>
       <section className={styles.hero}>
         <div className={styles.container}>
           <div className={styles.content}>
@@ -36,8 +42,130 @@ function Pricing() {
             Yearly
           </a>
         </div>
+        <div className={styles.plans}>
+          <div className={`${styles.plansCard} ${styles.startUp}`}>
+            <div className={styles.plansCardHeading}>
+              <div className={styles.plansCardTitle}>
+                <div className={styles.plansCardIcon}>
+                  <img src={startUpIcon} alt="star icon" />
+                </div>
+                <h3>Startup</h3>
+              </div>
+              <p>Perfect to get started</p>
+            </div>
+            <div className={styles.plansPricing}>
+              <div className={styles.plansPricingFigure}>
+                <div className={styles.plansPricingCurrency}>
+                  <img src={currency} alt="currency symbol" />
+                </div>
+                <h4>7,500</h4>
+              </div>
+              <p>per month</p>
+            </div>
+            <div className={styles.pricingFeatures}>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p>Ai-Powered call transcriptions</p>
+              </div>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p>Call Tracking & Recording</p>
+              </div>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p> Business Hours</p>
+              </div>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p>Email & Chat Support</p>
+              </div>
+            </div>
+            <Link href="/try"></Link>
+          </div>
+          <div className={`${styles.plansCard} ${styles.growing}`}>
+            <div className={styles.plansCardHeading}>
+              <div className={styles.plansCardTitle}>
+                <div className={styles.plansCardIcon}>
+                  <img src={growingIcon} alt="star icon" />
+                </div>
+                <h3>Growing</h3>
+              </div>
+              <p>Perfect to get started</p>
+            </div>
+            <div className={styles.plansPricing}>
+              <div className={styles.plansPricingFigure}>
+                <div className={styles.plansPricingCurrency}>
+                  <img src={currency} alt="currency symbol" />
+                </div>
+                <h4>7,500</h4>
+              </div>
+              <p>per month</p>
+            </div>
+            <div className={styles.pricingFeatures}>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p>Ai-Powered call transcriptions</p>
+              </div>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p>Call Tracking & Recording</p>
+              </div>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p> Business Hours</p>
+              </div>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p>Email & Chat Support</p>
+              </div>
+            </div>
+            <Link href="/try"></Link>
+          </div>
+          <div className={`${styles.plansCard} ${styles.enterprise}`}>
+            <div className={styles.plansCardHeading}>
+              <div className={styles.plansCardTitle}>
+                <div className={styles.plansCardIcon}>
+                  <img src={enterpriseIcon} alt="star icon" />
+                </div>
+                <h3>Startup</h3>
+              </div>
+              <p>Perfect to get started</p>
+            </div>
+            <div className={styles.plansPricing}>
+              <div className={styles.plansPricingFigure}>
+                <div className={styles.plansPricingCurrency}>
+                  <img src={currency} alt="currency symbol" />
+                </div>
+                <h4>7,500</h4>
+              </div>
+              <p>per month</p>
+            </div>
+            <div className={styles.pricingFeatures}>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p>Ai-Powered call transcriptions</p>
+              </div>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p>Call Tracking & Recording</p>
+              </div>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p> Business Hours</p>
+              </div>
+              <div className={styles.pricingFeaturesItem}>
+                <img src={checkIcon} alt="check-mark icon" />
+                <p>Email & Chat Support</p>
+              </div>
+            </div>
+            <Link href="/try"></Link>
+          </div>
+
+          <div className={`${styles.plansCard} ${styles.growing}`}></div>
+          <div className={`${styles.plansCard} ${styles.enterprise}`}></div>
+        </div>
       </section>
-    </>
+    </div>
   );
 }
 
