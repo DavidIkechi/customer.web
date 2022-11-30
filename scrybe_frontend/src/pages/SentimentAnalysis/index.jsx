39ac87c1bd1f5aa7363ae49a2950105a7990d290
tochibedford <musicofbyte@gmail.com>
2022-11-30 16:37:47 +0100

    chore: removed use of mockend with actual api

--- scrybe_frontend/src/pages/SentimentAnalysis/index.jsx
+++ scrybe_frontend/src/pages/SentimentAnalysis/index.jsx
@@ -10,11 +10,10 @@ import OverAllSentimentCard from "./components/OverallSentimentCard";
 import VerdictCard from "./components/VerdictCard";
 import PhraseTagCard from "./components/PhraseTagCard";
 import SideBar from "../../components/SideBar";
-import { useMockEnd, useMockAuthAndReadSentiment } from "./hooks";
+import { useMockAuthAndReadSentiment } from "./hooks";
 
 function SentimentAnalysis() {
   const [isMobileAsideOpen, setIsMobileAsideOpen] = useState(false);
-  const sentiment = useMockEnd(10);
   const params = useParams();
   const sentimentData = useMockAuthAndReadSentiment(parseInt(params.AudioId));
 
@@ -58,17 +57,6 @@ function SentimentAnalysis() {
     setIsMobileAsideOpen(false);
   };
 
-  const senti =
-    sentiment.map !== undefined
-      ? sentiment.map((data, index) => {
-          data.positivity_score = JSON.parse(data.sentiment_score)[0];
-          data.neutrality_score = JSON.parse(data.sentiment_score)[1];
-          data.negativity_score = JSON.parse(data.sentiment_score)[2];
-          data.transcript = data.transcription;
-          data.time = index * 30; //in seconds
-          return data;
-        })
-      : [];
   return (
     <SideBar>
       {console.log(sentimentData)}
