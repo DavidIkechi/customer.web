e5a72a148bdcea4ca72e98ddc6af6b08d8f85739
Oghenetega Esedere <arcteggzz@gmail.com>
2022-11-30 01:10:51 +0100

    feat: added audio playback functionality

--- scrybe_frontend/src/pages/TranscribePage/Transcribe/Components/TranscriptionMain/TranscriptionMain.jsx
+++ scrybe_frontend/src/pages/TranscribePage/Transcribe/Components/TranscriptionMain/TranscriptionMain.jsx
@@ -1,93 +1,124 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./TranscriptionMain.module.scss";
 // import TranscriptionsList from "./TranscriptionsList/TranscriptionsList";
 import Dummy from "./TranscriptionsList/test";
 import TranscriptionRightBar from "./TranscriptionRightBar/TranscriptionRightBar";
+import axios from "axios";
 
 function TranscriptionMain() {
-  // const [transcribedText, setTranscribedText] = useState("");
-  // const [formattedData, setFormattedData] = useState([]);
+  const [formattedData, setFormattedData] = useState([]);
 
-  // //get custom Id from current window url
-  // const getAudioID = () => {
-  //   const currentURL = window.location.href
-  //     .toString()
-  //     .split(window.location.host)[1];
-  //   const currentID = currentURL.substring(16, currentURL.length);
-  //   console.log(currentID);
-  //   return currentID;
-  // };
+  const [audioSrc, setAudioSrc] = useState("");
+  const audioElem = useRef();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [audioDuration, setAudioDuration] = useState(0);
+  const onLoadedMetadata = () => {
+    setAudioDuration(Math.round(audioElem.current?.duration));
+  };
+  const onTimeUpdate = () => {
+    setCurrentTime(Math.floor(audioElem.current.currentTime));
+  };
+  const updateCurrentTime = () => {
+    if (isPlaying) setCurrentTime(audioElem.current?.currentTime);
+  };
+  useEffect(() => {
+    if (isPlaying) {
+      audioElem.current.play();
+      updateCurrentTime();
+    } else audioElem.current.pause();
+  }, [isPlaying]);
+  useEffect(() => {
+    async function fetchAudio() {
+      const audioSrc =
+        "http://commondatastorage.googleapis.com/codeskulptor-demos/pyman_assets/ateapill.ogg";
+      setAudioSrc(audioSrc);
+    }
+    fetchAudio();
+  }, []);
+  const PlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
 
-  // //query backend to get transcribed data
-  // const loadTranscription = (str) => {
-  //   const apiReference = "";
-  //   fetch(`${apiReference}/${str}`)
-  //     .then((response) => response.json())
-  //     .then((data) => setTranscribedText(data));
-  // };
+  //fetch data from custom API for now.
+  const fetchData = () => {
+    axios
+      .get("https://api.mocki.io/v2/fc2a0cd8/transcription")
+      .then((newRes) => {
+        setFormattedData(generateArray(newRes.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  // const loadTest = () => {
-  //   const apiReference = "https://www.boredapi.com/api/activity";
-  //   fetch(`${apiReference}`)
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data));
-  // };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  // //cleanup the data gotten
-  // const generateArray = (str) => {
-  //   const cleanedData = [];
-  //   const wordArray = str.split(" ");
-  //   let time = -30;
-  //   let objectID = -1;
-  //   let emptyString = "";
-  //   let counter = 0;
-  //   console.log(wordArray);
+  const generateArray = (str) => {
+    const cleanedData = [];
 
-  //   wordArray.map((word) => {
-  //     if (counter < 30) {
-  //       emptyString = emptyString + " " + word;
-  //       counter++;
-  //     }
-  //     if (counter == 30) {
-  //       objectID++;
-  //       time = time + 30;
-  //       const formatedTime = timeFormatter(time);
-  //       const object = {
-  //         id: objectID,
-  //         timeCount: formatedTime,
-  //         stringText: emptyString,
-  //       };
-  //       cleanedData.push(object);
-  //       emptyString = "";
-  //       counter = 0;
-  //     }
-  //   });
+    const wordArray = str.split(" ");
+    let time = -30;
+    let objectID = -1;
+    let emptyString = "";
+    let counter = 0;
 
-  //   return cleanedData;
-  // };
+    wordArray.map((word) => {
+      if (counter < 50) {
+        emptyString = emptyString + " " + word;
+        counter++;
+      }
+      if (counter == 50) {
+        objectID++;
+        time = time + 30;
+        const formatedTime = timeFormatter(time);
+        const object = {
+          id: objectID,
+          timeCount: formatedTime,
+          stringText: emptyString,
+        };
+        cleanedData.push(object);
+        emptyString = "";
+        counter = 0;
+      }
+    });
 
-  // //format time function
-  // const timeFormatter = (num) => {
-  //   const divisor_for_minutes = num % (60 * 60);
-  //   const minutes = Math.floor(divisor_for_minutes / 60);
-  //   const divisor_for_seconds = divisor_for_minutes % 60;
-  //   const seconds = Math.ceil(divisor_for_seconds);
-  //   const formatedTime = `${minutes}:${seconds}`;
-  //   if (formatedTime.length === 4) return `0${minutes}:${seconds}`;
-  //   if (formatedTime.length === 3) return `0${minutes}:${seconds}0`;
-  // };
+    return cleanedData;
+  };
 
-  // useEffect(() => {
-  //   loadTest();
-  //   // const idQuery = getAudioID();
-  //   // loadTranscription(idQuery);
-  //   // setFormattedData(generateArray(transcribedText));
-  // }, []);
-
+  // format time function
+  const timeFormatter = (num) => {
+    if (num === 0 || isNaN(num)) return `00:00`;
+    const divisor_for_minutes = num % (60 * 60);
+    const minutes = Math.floor(divisor_for_minutes / 60);
+    const divisor_for_seconds = divisor_for_minutes % 60;
+    const seconds = Math.ceil(divisor_for_seconds);
+    const formatedTime = `${minutes}:${seconds}`;
+    if (formatedTime.length === 3 && minutes < 1)
+      return `0${minutes}:0${seconds}`;
+    if (formatedTime.length === 4) return `0${minutes}:${seconds}`;
+    if (formatedTime.length === 3 && minutes > 0)
+      return `0${minutes}:${seconds}0`;
+  };
   return (
     <div className={styles.TranscriptionMain}>
-      <Dummy />
-      <TranscriptionRightBar />
+      <Dummy formattedData={formattedData} />
+      <button onClick={PlayPause}>Click me</button>
+      <TranscriptionRightBar
+        audioElem={audioElem}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        audioDuration={audioDuration}
+        currentTime={currentTime}
+      />
+      <audio
+        src={audioSrc}
+        ref={audioElem}
+        onLoadedMetadata={onLoadedMetadata}
+        onTimeUpdate={onTimeUpdate}
+      />
     </div>
   );
 }
--- scrybe_frontend/src/pages/TranscribePage/Transcribe/Components/TranscriptionMain/TranscriptionRightBar/TranscriptionRightBar.jsx
+++ scrybe_frontend/src/pages/TranscribePage/Transcribe/Components/TranscriptionMain/TranscriptionRightBar/TranscriptionRightBar.jsx
@@ -4,10 +4,22 @@ import AudioControl from "./Components/AudioControl";
 import SentimentAnalysis from "./Components/SentimentAnalysis";
 import RecordingList from "./Components/RecordingList";
 
-function TranscriptionRightBar() {
+function TranscriptionRightBar({
+  audioElem,
+  setIsPlaying,
+  isPlaying,
+  audioDuration,
+  currentTime,
+}) {
   return (
     <div className={styles.TranscriptionRightBar}>
-      <AudioControl />
+      <AudioControl
+        audioElem={audioElem}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        audioDuration={audioDuration}
+        currentTime={currentTime}
+      />
       <SentimentAnalysis />
       <RecordingList />
     </div>
