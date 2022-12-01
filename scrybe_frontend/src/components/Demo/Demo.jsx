d21c0c50352be54e64329d40107b811874271302
VoN <hi@imrvon.com>
2022-12-01 14:21:51 +0100

    Demo Page Added

---
+++ scrybe_frontend/src/components/Demo/Demo.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Demo() {
+  return (
+    <>
+        <div className="demo-container">
+            <div>
+                
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    
+    </>
+  )
+}
+
+export default Demo
