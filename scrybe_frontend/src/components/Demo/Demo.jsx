2933c11fa90ff02545b1b05e5705d148a578d7ff
VoN <hi@imrvon.com>
2022-12-01 14:22:35 +0100

    Demo Page Added

--- scrybe_frontend/src/components/Demo/Demo.jsx
+++ scrybe_frontend/src/components/Demo/Demo.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Demo() {
   return (
     <>
-        <div className="demo-container">
-            <div>
-                
-            </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    
+      <div className="demo-container">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Demo
+export default Demo;
