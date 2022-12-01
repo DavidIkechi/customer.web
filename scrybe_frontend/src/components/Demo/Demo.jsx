92b5bf4b344fc27172635b94798c12a1f4365bac
VoN <hi@imrvon.com>
2022-12-01 22:13:30 +0100

    Still Demo Page

--- scrybe_frontend/src/components/Demo/Demo.jsx
+++ scrybe_frontend/src/components/Demo/Demo.jsx
@@ -30,10 +30,10 @@ function Demo() {
           <div>
             <img src="" alt="" />
           </div>
-          <h2>Recording Overview</h2>
+          <h2>Total Recordings</h2>
           <p>
-            Your audio uploads are organized by Date of Upload and maximum
-            upload size is 50mb
+            Barcharts help you check record of your total uploads; weekly and
+            monthly
           </p>
           <button>PREVIOUS</button>
           <button>NEXT</button>
@@ -43,11 +43,8 @@ function Demo() {
           <div>
             <img src="" alt="" />
           </div>
-          <h2>Total Recordings</h2>
-          <p>
-            Barcharts help you check record of your total uploads; weekly and
-            monthly
-          </p>
+          <h2>Total Analysis</h2>
+          <p>Full analysis of your total recording uploads</p>
           <button>PREVIOUS</button>
           <button>NEXT</button>
         </div>
@@ -56,21 +53,103 @@ function Demo() {
           <div>
             <img src="" alt="" />
           </div>
-          <h2>Total Recordings</h2>
+          <h2>Agent's Leaderboard</h2>
           <p>
-            Barcharts help you check record of your total uploads; weekly and
-            monthly
+            Consists of the amount of agents in your team. Your plan determines
+            the amount of agents you can have.
           </p>
           <button>PREVIOUS</button>
           <button>NEXT</button>
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+
+        <div className="demo-6">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <h2>Transcription</h2>
+          <p>
+            Audios are transcribed into break points for easy access
+            clarifications
+          </p>
+          <button>PREVIOUS</button>
+          <button>NEXT</button>
+        </div>
+
+        <div className="demo-7">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <h2>Multi - Recordings</h2>
+          <p>You can access your previous recordings with just one click.</p>
+          <button>PREVIOUS</button>
+          <button>NEXT</button>
+        </div>
+
+        <div className="demo-8">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <h2>Sentiment Analysis</h2>
+          <p>You can access your previous recordings with just one click.</p>
+          <button>PREVIOUS</button>
+          <button>NEXT</button>
+        </div>
+
+        <div className="demo-9">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <h2>Transcription</h2>
+          <p>
+            Sentiment analysis into three points: Positive, Negative and
+            Neutral.
+          </p>
+          <button>PREVIOUS</button>
+          <button>NEXT</button>
+        </div>
+
+        <div className="demo-10">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <h2>Overall Sentiment</h2>
+          <p>
+            Access to quantitive break down of overall sentiment based on audio
+            uploaded
+          </p>
+          <button>PREVIOUS</button>
+          <button>NEXT</button>
+        </div>
+
+        <div className="demo-11">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <h2>Customer Satisfaction bar</h2>
+          <p>Customer satisfaction automatically measured and accessed.</p>
+          <button>PREVIOUS</button>
+          <button>NEXT</button>
+        </div>
+
+        <div className="demo-12">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <h2>Download Categories</h2>
+          <p>Download verdict in your preferred format: pdf, html, Json.</p>
+          <button>PREVIOUS</button>
+          <button>NEXT</button>
+        </div>
+
+        <div className="demo-13">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <h2>TRIAL COMPLETE</h2>
+          <p>What next? You can test it out using the button below.</p>
+          <button>REPLAY</button>
+          <button>START TRIAL</button>
+        </div>
       </div>
     </>
   );
