8d4d010ab93915d425110969792b54459741ef88
VoN <hi@imrvon.com>
2022-12-01 15:19:30 +0100

    Demo Page - HTML

--- scrybe_frontend/src/components/Demo/Demo.jsx
+++ scrybe_frontend/src/components/Demo/Demo.jsx
@@ -4,11 +4,44 @@ function Demo() {
   return (
     <>
       <div className="demo-container">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="demo-1">
+            <div><img src="" alt="" /></div>
+            <h2>Welome to the Tour</h2>
+            <p>Click on Next button to continue</p>
+            <button>NEXT</button>
+        </div>
+
+        <div className="demo-2">
+            <div><img src="" alt="" /></div>
+            <h2>Recording Overview</h2>
+            <p>Your audio uploads are organized by Date of Upload and maximum upload size is 50mb</p>
+            <button>PREVIOUS</button>
+            <button>NEXT</button>
+        </div>
+
+        <div className="demo-3">
+            <div><img src="" alt="" /></div>
+            <h2>Recording Overview</h2>
+            <p>Your audio uploads are organized by Date of Upload and maximum upload size is 50mb</p>
+            <button>PREVIOUS</button>
+            <button>NEXT</button>
+        </div>
+
+        <div className="demo-4">
+            <div><img src="" alt="" /></div>
+            <h2>Total Recordings</h2>
+            <p>Barcharts help you check record of your total uploads; weekly and monthly</p>
+            <button>PREVIOUS</button>
+            <button>NEXT</button>
+        </div>
+
+        <div className="demo-5">
+            <div><img src="" alt="" /></div>
+            <h2>Total Recordings</h2>
+            <p>Barcharts help you check record of your total uploads; weekly and monthly</p>
+            <button>PREVIOUS</button>
+            <button>NEXT</button>
+        </div>
         <div></div>
         <div></div>
         <div></div>
