e4e907396ac28828bbd676f92847d2e64b6d0949
Sammydepoj <samueladepoju86@yahoo.com>
2022-12-02 14:31:47 +0100

    navbar component added to sales team landing page

--- scrybe_frontend/src/pages/NewLandingPageSalesTeam/index.jsx
+++ scrybe_frontend/src/pages/NewLandingPageSalesTeam/index.jsx
@@ -5,9 +5,11 @@ import HowItWorks from "./HowItWorks/index";
 import WhoWeServe from "./WhoWeServe/index";
 import Subscribe from "./Subscribe";
 import Footer from "../../components/footer/index";
+import Navbar from "../../components/navBar_free/index";
 const NewLandingPageSalesTeam = () => {
   return (
     <div>
+      <Navbar />
       <Hero />
       <Help />
       <HowItWorks />
