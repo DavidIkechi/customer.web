0fd68d3d5510faa99529c21515b57e2b317d947e
Bsaint95 <bsaintdesigns@gmail.com>
2022-12-03 14:00:42 +0100

    Agent leaderboard weight

--- backend/app/db.py
+++ backend/app/db.py
@@ -21,16 +21,16 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_PASS = os.getenv("DB_PASS")
 DB_USER = os.getenv("DB_USER")
-"""
+
 DB_CONNECTION = DB_USER+":"+DB_PASS+"@"+DB_HOST+"/"+DB_NAME
 SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://"+DB_CONNECTION
-"""
-SQLALCHEMY_DATABASE_URL = "sqlite:///./hd.db"
+
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./hd.db"
 
 
 engine = create_engine(
 
-    SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
