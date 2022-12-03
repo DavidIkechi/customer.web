e635d62e6f1f49f8cd761aba1b08f727a44debf7
Victoria <adedayovicky123@gmail.com>
2022-12-03 18:45:41 +0100

    feat(157) connected to upload new file modal

--- scrybe_frontend/src/pages/DashboardOverview/components/RecentRecording.jsx
+++ scrybe_frontend/src/pages/DashboardOverview/components/RecentRecording.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import { useMockAuthAndGetRecording } from "../hooks";
 import styles from "../DashboardOverview.module.scss";
 import toneWave from "../assets/tone_wave.svg";
@@ -107,9 +109,9 @@ const RecentRecording = () => {
               Start uploading agent recordings to get an overview of your team’s
               performance.
             </p>
-            <button className={styles.empty_state_btn}>
+            <Link to="/upload-new-file" className={styles.empty_state_btn}>
               <img src={upload} alt="upload" /> Upload
-            </button>
+            </Link>
           </div>
           <div className={styles.empty_state_desktop}>
             <img src={empty_state} alt="No activity found" />
@@ -119,9 +121,9 @@ const RecentRecording = () => {
               below to upload a recording and begin your transcription and
               sentiment analysis.
             </p>
-            <button className={styles.empty_state_btn}>
+            <Link to="/upload-new-file" className={styles.empty_state_btn}>
               <img src={upload} alt="upload" /> Upload
-            </button>
+            </Link>
           </div>
         </div>
       )}
