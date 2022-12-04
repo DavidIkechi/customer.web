b9785e2ad0973eb8d319e3edc3d91e82718576f7
dprincecoder <dprincecoder@gmail.com>
2022-12-04 14:53:30 +0100

    fix error in import

--- scrybe_frontend/src/pages/AgentReport/index.jsx
+++ scrybe_frontend/src/pages/AgentReport/index.jsx
@@ -4,23 +4,24 @@ import AgentRecordings from "./components/AgentRecordings";
 import Charts from "./components/ChartContainer";
 import styles from "./styles/AgentReport.module.scss";
 
-import Sidebar from "../../components/NewDesignSideBar/index";
-import TopNav from "../../components/TopNav/index";
+import NewDesignSidebar from "../../components/NewDesignSidebar";
+import TopNav from "../../components/TopNav";
 
 function AgentReport() {
   return (
     <>
       <TopNav />
-      <Sidebar />
-      <div className={styles.container}>
-        <div className={styles.mainWrapper}>
-          <div className={styles.topDetailsDiv}>
-            <Charts />
-            <AgentDetails />
+      <NewDesignSidebar>
+        <div className={styles.container}>
+          <div className={styles.mainWrapper}>
+            <div className={styles.topDetailsDiv}>
+              <Charts />
+              <AgentDetails />
+            </div>
+            <AgentRecordings />
           </div>
-          <AgentRecordings />
         </div>
-      </div>
+      </NewDesignSidebar>
     </>
   );
 }
