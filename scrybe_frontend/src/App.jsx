f78b5997b406a732d18ca5f0a226edbf31f3ca10
dprincecoder <dprincecoder@gmail.com>
2022-12-04 04:12:34 +0100

    add new homepage

--- scrybe_frontend/src/App.jsx
+++ scrybe_frontend/src/App.jsx
@@ -53,7 +53,7 @@ import { UploadModal } from "./pages/UploadRecordingsModal";
 import Error from "./pages/Error/Error";
 import Von from "./pages/Von/Von";
 import LandingPageSalesTeam from "./pages/NewLandingPageSalesTeam/index";
-// import HomePageRedesign from "./Pages/HomePageRedesign";
+import HomePageRedesign from "./pages/HomePageRedesign";
 import WithAuth from "./HOCs";
 import Support from "./pages/Support/support";
 
@@ -73,7 +73,7 @@ function App() {
         {/* access without sign in */}
         {/* <Route path="/verification" element={<EmailVerify />} /> */}
         {/* <Route path="/logout" element={<Logout />} /> */}
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePageRedesign />} />
         <Route path="/try" element={<TryForFree />} />
         <Route path="/careers" element={<Careers />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
