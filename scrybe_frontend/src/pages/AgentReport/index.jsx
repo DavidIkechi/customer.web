4840cedeb29d0785b06f0d4fbf8e0d46dd98e008
Adepoju Adeyemi Samuel <samueladepoju86@yahoo.com>
2022-12-04 07:06:59 +0100

    Feat:changed agent report page sidebar to the new one.

--- scrybe_frontend/src/pages/AgentReport/index.jsx
+++ scrybe_frontend/src/pages/AgentReport/index.jsx
@@ -4,7 +4,7 @@ import AgentRecordings from "./components/AgentRecordings";
 import Charts from "./components/ChartContainer";
 import styles from "./styles/AgentReport.module.scss";
 
-import Sidebar from "../../components/SideBar/index";
+import Sidebar from "../../components/NewDesignSideBar/index";
 
 function AgentReport() {
   return (
