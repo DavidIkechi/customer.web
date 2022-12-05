6faa56a6295a46a6fb9accc4f30dabd0315844c6
Abiola Fadeyi <abiolafadeyi10@gmail.com>
2022-12-05 11:14:43 -0800

    modified agent performance to generate for month

--- backend/app/db.py
+++ backend/app/db.py
@@ -22,19 +22,14 @@ DB_NAME = os.getenv("DB_NAME")
 DB_PASS = os.getenv("DB_PASS")
 DB_USER = os.getenv("DB_USER")
 
-DB_CONNECTION = DB_USER+":"+DB_PASS+"@"+DB_HOST+"/"+DB_NAME
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://"+DB_CONNECTION
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# DB_CONNECTION = DB_USER+":"+DB_PASS+"@"+DB_HOST+"/"+DB_NAME
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://"+DB_CONNECTION
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 
 
 engine = create_engine(
-<<<<<<< HEAD
-    SQLALCHEMY_DATABASE_URL#, connect_args = {"check_same_thread": False}
-=======
-    SQLALCHEMY_DATABASE_URL
-
->>>>>>> 0a1aaa818acc5a5c1be0cd699b8cf4dc1ce81e87
+    SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
