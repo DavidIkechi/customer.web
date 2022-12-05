28f862b9d2c4b5633bc377c1d7e4ce1585575d9d
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-12-05 11:58:25 +0100

    chore: Implement form validation for Change Password

--- scrybe_frontend/src/pages/Settings/SettingsPageSubPages/AccountSettings/AccountSettings.jsx
+++ scrybe_frontend/src/pages/Settings/SettingsPageSubPages/AccountSettings/AccountSettings.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import Footer from "../../../../components/Footer";
 import RedirectNav from "../../Components/SettingsPageRedirectNav/SettingsPageRedirectNav";
 import AccountPageCss from "./AccountSettings.module.scss";
 
 const AccountSettings = () => {
   const currentDate = new Date().toLocaleDateString("en-GB");
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  // Watch event for disable button
+  const password = watch("password");
+  const confirmPassword = watch("confirmPassword");
+
+  const isValid = password && confirmPassword;
 
   return (
     <div className="">
@@ -15,24 +29,51 @@ const AccountSettings = () => {
           <h2>Account security</h2>
           <p>Change your password</p>
         </div>
-        <form action="" className={AccountPageCss.form}>
+        <form className={AccountPageCss.form}>
           <div className={AccountPageCss.formGroup}>
             <label htmlFor="Password">Enter new password:</label>
             <input
               type="password"
+              name="password"
               id="Password"
+              className={`${errors.password && AccountPageCss.errorInput} `}
               placeholder="Enter a new password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Password must be at least 8 characters",
+                },
+              })}
             />
+            <p className={AccountPageCss.errorMsg}>
+              {errors.password?.message}
+            </p>
           </div>
           <div className={AccountPageCss.formGroup}>
             <label htmlFor="confirmPassword">Retype password:</label>
             <input
               type="password"
+              name="confirmPassword"
               id="confirmPassword"
               placeholder="Enter Password again"
+              {...register("confirmPassword", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Password inputted did not match",
+                },
+              })}
             />
+            <p className={AccountPageCss.errorMsg}>
+              {errors.confirmPassword?.message}
+            </p>
           </div>
-          <div className={AccountPageCss.formSubmit}>
+          <div
+            className={`${!isValid && AccountPageCss.formSubmit} ${
+              isValid && AccountPageCss.submitValid
+            }`}
+          >
             <button type="submit">Save changes</button>
           </div>
         </form>
