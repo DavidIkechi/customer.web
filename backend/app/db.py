70a5dff7acf696683207d07534b524c236c0018e
Abiola Fadeyi <abiolafadeyi10@gmail.com>
2022-12-05 20:05:39 -0800

    changes

--- backend/app/db.py
+++ backend/app/db.py
@@ -25,12 +25,12 @@ DB_USER = os.getenv("DB_USER")
 DB_CONNECTION = DB_USER+":"+DB_PASS+"@"+DB_HOST+"/"+DB_NAME
 SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://"+DB_CONNECTION
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./htest.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./htest.db"
 
 
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL#, connect_args={"check_same_thread": False}
 
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
