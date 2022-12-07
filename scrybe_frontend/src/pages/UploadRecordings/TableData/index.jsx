30cf013b205b7893a12744c3062a9405db4c7b1a
dprincecoder <dprincecoder@gmail.com>
2022-12-07 16:16:57 +0100

    fix

--- scrybe_frontend/src/pages/UploadRecordings/TableData/index.jsx
+++ scrybe_frontend/src/pages/UploadRecordings/TableData/index.jsx
@@ -32,10 +32,6 @@ const TableData = ({ searchKeyword }) => {
 
   // const timeLeft = 20;
 
-  // useEffect(() => {
-  //   const newRecordings = fetchData("list-audios-by-user");
-  //   setAllRecordings([newRecordings.data]);
-  // }, []);
 
   const getChecked = (e) => {
     let checkedList = [...recordCheckedList];
