0d2eb11cc5e16c0e0e72fd4b9d0ce3745e9ba206
Oghenetega Esedere <arcteggzz@gmail.com>
2022-12-08 00:15:37 +0100

    Feat/fro 102 transcription page (#642)
    * feat: added new top bar
    
    * fix:added correct sidebar
    
    * feat:download transcription as JSON
    
    * fix:deleted unused files, added png imgs, added audio_size formatter
    
    * fix:recent recordings
    
    * fix: styling issues
    
    * feat: fixes for transcription page
    
    * fix:eslint

--- scrybe_frontend/src/pages/TranscribePage/audioFiles/songsData.js
+++
@@ -1,8 +0,0 @@
-const songsdata = [
-  {
-    title: "Drake - Forever",
-    url: "http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a",
-  },
-];
-
-export default songsdata;
