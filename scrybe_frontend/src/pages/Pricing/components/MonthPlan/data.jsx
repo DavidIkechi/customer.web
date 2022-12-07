e30b247818bf5694d68184c72b1acb7af79f76da
damolabolaji <bolaji.oyindamola@gmail.com>
2022-12-07 16:25:11 +0100

    refactor: month and year pricing components

---
+++ scrybe_frontend/src/pages/Pricing/components/MonthPlan/data.jsx
@@ -0,0 +1,45 @@
+export const monthlyPricing = [
+  {
+    id: 1,
+    title: "Startup",
+    headDescription: "Perfect to get started",
+    pricing: "7,500",
+    duration: "per month",
+    features: [
+      "Ai-Powered call transcriptions",
+      "Call Tracking & Recording",
+      " Business Hours",
+      "Email & Chat Support",
+    ],
+  },
+
+  {
+    id: 2,
+    title: "Growing",
+    headDescription: "Make individual products",
+    pricing: "13,500",
+    duration: "per month",
+    features: [
+      "Everything in Startup, plus:",
+      "API & Webhook Access",
+      "Custom Workflows",
+      "Dedicated Account Manager",
+      "Priority Support",
+    ],
+  },
+  {
+    id: 3,
+    title: "Enterprise",
+    headDescription: "For big companies",
+    pricing: "24,000",
+    duration: "per month",
+    features: [
+      "Everything in Growing, plus:",
+      "Professional Services",
+      "Custom Analytics & Reports",
+      "Personalized Onboarding Support",
+      "Agent Coaching & Call Scoring",
+      "Dedicated Technical Support",
+    ],
+  },
+];
---
+++ scrybe_frontend/src/pages/Pricing/components/YearPlan/data.jsx
@@ -0,0 +1,45 @@
+export const yearlyPricing = [
+  {
+    id: 1,
+    title: "Startup",
+    headDescription: "Perfect to get started",
+    pricing: "6,500",
+    duration: "per month",
+    features: [
+      "Ai-Powered call transcriptions",
+      "Call Tracking & Recording",
+      " Business Hours",
+      "Email & Chat Support",
+    ],
+  },
+
+  {
+    id: 2,
+    title: "Growing",
+    headDescription: "Make individual products",
+    pricing: "10,000",
+    duration: "per month",
+    features: [
+      "Everything in Startup, plus:",
+      "API & Webhook Access",
+      "Custom Workflows",
+      "Dedicated Account Manager",
+      "Priority Support",
+    ],
+  },
+  {
+    id: 3,
+    title: "Enterprise",
+    headDescription: "For big companies",
+    pricing: "21,000",
+    duration: "per month",
+    features: [
+      "Everything in Growing, plus:",
+      "Professional Services",
+      "Custom Analytics & Reports",
+      "Personalized Onboarding Support",
+      "Agent Coaching & Call Scoring",
+      "Dedicated Technical Support",
+    ],
+  },
+];
