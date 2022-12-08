fd00b7c7e3491a70d11fdee18da04fde585074bb
Priceless001 <ayenpre@gmail.com>
2022-12-08 12:19:13 +0100

    fix and changes

--- scrybe_frontend/src/pages/VerificationEmail/EmailVerify.jsx
+++ scrybe_frontend/src/pages/VerificationEmail/EmailVerify.jsx
@@ -18,13 +18,12 @@ const EmailVerify = () => {
   const handleSubmit = async () => {
     try {
       // let token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImF5ZW5wcmVAZ21haWwuY29tIn0.LSsSIjvkCHI5tfg2Hj3Q3Ov2MwFrNFdn6W9jC2UXo4Q";
-      const response = await axios.get(
-        `https://api.heed.hng.tech/verification?token=${token}`
-      );
-      console.log(response.data);
-      setMessage(response.data);
+      const response = await axios.get(`verification?token=${token}`);
+      console.log(response.data.data);
+      setMessage(response.data.data);
     } catch (err) {
-      console.log(err);
+      console.log(err.response.data.detail);
+      setMessage(err.response.data.detail);
     }
   };
   return (
