c125c13960e60fd499798303121a365f9672fdb0
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-12-08 11:16:59 +0100

    Merge branch 'dev' of github.com:workshopapps/customersupport.web into feat/FRO-124-HEED-SETTINGS

--- scrybe_frontend/src/components/TopNav/DropdownMenu/index.jsx
+++ scrybe_frontend/src/components/TopNav/DropdownMenu/index.jsx
@@ -1,10 +1,26 @@
+import axios from "axios";
 import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import account from "./assets/account.jpg";
 import logout from "./assets/logout.jpg";
 import support from "./assets/support.jpg";
 import styles from "./dropdown.module.scss";
 function DropDownModal({ closeModal }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const getUserAccount = async () => {
+    const config = {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("heedAccessToken")}`,
+      },
+    };
+    const res = await axios.get("account", config);
+    setCurrentUser(res.data);
+  };
+  useEffect(() => {
+    getUserAccount();
+  }, []);
   const signout = () => {
     Cookies.remove("heedAccessToken");
     localStorage.removeItem("heedAccessToken");
@@ -26,12 +42,14 @@ function DropDownModal({ closeModal }) {
         <img src={support} alt="" />
         <p className={styles.p}>Support</p>
       </div>
-      <div className={styles.list}>
-        <img src={logout} alt="" />
-        <Link to="/signin" className={styles.p} onClick={signout}>
-          Logout
-        </Link>
-      </div>
+      {currentUser && (
+        <div className={styles.list}>
+          <img src={logout} alt="" />
+          <Link to="/signin" className={styles.p} onClick={signout}>
+            Logout
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
