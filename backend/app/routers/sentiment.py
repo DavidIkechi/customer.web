644996c0eb2cbb72f7ab304e5a3e6989159d5a79
David Akwuru <daakwuru@gmail.com>
2022-12-09 17:06:55 +0000

    Update sentiment.py

--- backend/app/routers/sentiment.py
+++ backend/app/routers/sentiment.py
@@ -4,6 +4,12 @@ from nltk.tokenize import sent_tokenize
 from textblob import TextBlob
 import json
 
+
+def unique(list1):
+    npArray1 = np.array(list1)
+    uniqueNpArray1 = np.unique(npArray1)
+    return uniqueNpArray.tolist()
+
 def sentiment(transcript):
     nltk.download('vader_lexicon')
     nltk.download('punkt')
@@ -12,7 +18,7 @@ def sentiment(transcript):
     postive_sentences = []
     sentiment = SentimentIntensityAnalyzer().polarity_scores(transcript)
     #getting the most positive and negative sentences
-    for sentence in transcript.split():
+    for sentence in sentences:
         cleaned_word = TextBlob(sentence)
 
         individual_sentiment = cleaned_word.sentiment.polarity
@@ -20,7 +26,7 @@ def sentiment(transcript):
             negative_sentences.append({"sentence": sentence, "negativity_score": individual_sentiment})
         elif individual_sentiment >= 0.05:
             postive_sentences.append({"sentence": sentence, "positivity_score": individual_sentiment})
-    most_negative_sentences =json.dumps(negative_sentences)
+    most_negative_sentences =json.dumps(negative_sentences))
     most_postive_sentences = json.dumps(postive_sentences)
     result = {
         "Negative": sentiment['neg'],
