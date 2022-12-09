d77186835284dec4b800c20ff19948e471f98bac
GREENFONTS <godwillonyewuchii@gmail.com>
2022-12-09 18:40:01 +0100

    feat: implemented refresh token functionality

--- scrybe_frontend/src/helpers/axioshelp/axios.js
+++ scrybe_frontend/src/helpers/axioshelp/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { RefreshToken } from "./Utils/RefershToken";
 
-const baseURL = "https://api.heed.hng.tech/";
+export const baseURL = "https://api.heed.hng.tech/";
 
 const headers = {
   "Access-Control-Allow-Origin": "*",
@@ -27,6 +28,25 @@ axiosInstance.interceptors.request.use(
     return config;
   },
   (err) => Promise.reject(err)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async function (error) {
+    const originalRequest = error.config;
+    if (error?.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const data = await RefreshToken();
+      localStorage.setItem("heedAccessToken", data.access_token);
+      localStorage.setItem("heedRefreshToken", data.refresh_token);
+      axios.defaults.headers.common["Authorization"] =
+        "Bearer " + data.access_token;
+      return axiosInstance(originalRequest);
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
