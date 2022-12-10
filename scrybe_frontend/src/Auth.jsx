7f3d4ee5eabb85c9ad5177de5a37833382e8b22c
tochibedford <musicofbyte@gmail.com>
2022-12-10 11:45:47 +0100

    fix: account for expired token in authentication

--- scrybe_frontend/src/Auth.jsx
+++ scrybe_frontend/src/Auth.jsx
@@ -1,13 +1,26 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
+
 export const fetchToken = () => {
-  return localStorage.getItem("heedAccessToken");
+  return {
+    token: localStorage.getItem("heedAccessToken"),
+    activationTime: localStorage.getItem("accessTokenActivationTime"),
+  };
 };
 
 export function RequireToken() {
   let auth = fetchToken();
   let location = useLocation();
+  const tokenExpirationTime = 120; //in minutes
 
-  if (!auth) {
+  if (
+    // if there is no token or no activation time or (there is an activationTime but it is greater than 120 mins) then redirect to login page
+    !auth.token ||
+    !auth.activationTime ||
+    (auth.activationTime
+      ? (new Date().getTime() - auth.activationTime) / 60000 >=
+        tokenExpirationTime
+      : 0)
+  ) {
     return <Navigate to="/login" state={{ from: location }} />;
   }
 
