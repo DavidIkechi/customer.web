a015ddaf9fcbb2240ce6bf7baf066d9534645653
dprincecoder <dprincecoder@gmail.com>
2022-12-27 04:58:18 +0100

    rename

--- scrybe_frontend/src/Auth.jsx
+++ scrybe_frontend/src/Auth.jsx
@@ -1,10 +1,8 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { localStorageUser } from "./helpers/localStorageUser";
 
 export function RequireToken() {
-  //let auth =   localStorageUser() {name: "user", token: "token"}};
-  let auth = { name: "user", token: "token" };
-  let location = useLocation();
+  let auth = localStorageUser();
   if (!auth) {
     return <Navigate to="/login" replace />;
   }
