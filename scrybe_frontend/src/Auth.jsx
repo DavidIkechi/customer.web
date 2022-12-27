ea5040209ac13be4b333ae8d1f1853b2baea53a8
Dprincecoder <78590038+dprincecoder@users.noreply.github.com>
2022-12-27 05:49:40 +0100

    Update Auth.jsx

--- scrybe_frontend/src/Auth.jsx
+++ scrybe_frontend/src/Auth.jsx
@@ -2,8 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { localStorageUser } from "./helpers/localStorageUser";
 
 export function RequireToken() {
-  // let auth = localStorageUser();
-  let auth = { username: "test", token: "test" };
+   let auth = localStorageUser();
   if (!auth) {
     return <Navigate to="/login" replace />;
   }
