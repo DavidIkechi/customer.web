d7c92fb766ddc8b7a68d4c0d17cf88c538838c2b
David Akwuru <daakwuru@gmail.com>
2023-01-06 01:22:55 +0000

    adjusted the transcription mail crud

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -2,25 +2,24 @@ from rocketry import Rocketry
 from rocketry.conds import (
     every, hourly, daily,
     after_success,
-    true, false
+    true, false, cron
 )
 
 import cron_status
 
 cron_schedule = Rocketry(config={"task_execution": "async"})
 
-# Creating some tasks
-@cron_schedule.task(every("15 seconds"))
-async def do_things():
+@cron_schedule.task(every("3 seconds"))
+async def another():
+    print("dash")
+    await cron_status.check_and_update_jobs()  
+
+@cron_schedule.task(cron("* * * * *")) 
+async def constantly():
+    print("man")
     await cron_status.transcription_mail()
-    
 
-@cron_schedule.task(after_success(do_things))
-async def do_after():
-    await cron_status.check_and_update_jobs()
-
-    
-    
+      
 if __name__ == "__main__":
     # If this script is run, only Rocketry is run
     cron_schedule.run()
