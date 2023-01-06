ed0c39a9ec28e7f302536294b78a375354475b31
GREENFONTS <godwillonyewuchii@gmail.com>
2023-01-06 17:03:10 +0100

    chore: private routes setup

--- scrybe_frontend/src/Auth.jsx
+++ scrybe_frontend/src/Auth.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
+import NewDesignSideBar from "./components/NewDesignSidebar";
+import TopNav from "./components/TopNav";
+import styles from "./pages/DashboardOverview/DashboardOverview.module.scss";
 
 export function RequireToken() {
   const auth = useSelector((state) => state.user.token);
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [setIsSearching] = useState("");
+
+  const setterFn = (e) => {
+    setIsSearching(e.target.value);
+  };
 
   if (!auth) {
     return <Navigate to="/login" replace />;
   }
 
-  return <Outlet />;
+  return (
+    <div className={`${styles.dashboard_overviewParent} `}>
+      <NewDesignSideBar
+        toggleSidebar={toggleSidebar}
+        needSearchMobile="needSearchMobile"
+        getValue={(e) => setterFn(e)}
+        closeSidebar={() => setToggleSidebar(!toggleSidebar)}
+      >
+        <div className={styles.dashboard_overviewCol}>
+          <div className={styles.uploadedRecordingsSideBar}>
+            <TopNav
+              openSidebar={() => {
+                setToggleSidebar(!toggleSidebar);
+              }}
+              search={(e) => setterFn(e)}
+            />
+          </div>
+          <Outlet />
+        </div>
+      </NewDesignSideBar>
+    </div>
+  );
 }
 
 export function Authenticated() {
--- scrybe_frontend/src/pages/SentimentAnalysis/index.jsx
+++ scrybe_frontend/src/pages/SentimentAnalysis/index.jsx
@@ -9,14 +9,10 @@ import PhraseTagCard from "./components/PhraseTagCard";
 import SentimentAside from "./components/SentimentAside";
 import VerdictCard from "./components/VerdictCard";
 import styles from "./SentimentAnalysis.module.scss";
-// import SideBar from "../../components/SideBar";
-import NewDesignSideBar from "../../components/NewDesignSidebar";
-import TopNav from "../../components/TopNav";
 import { useReadSentiment } from "./hooks";
 
 function SentimentAnalysis() {
   const [isMobileAsideOpen, setIsMobileAsideOpen] = useState(false);
-  const [toggleSidebar, setToggleSidebar] = useState(false);
   const params = useParams();
   const sentimentData = useReadSentiment(params.AudioId);
   const positiveTags = [
@@ -60,83 +56,68 @@ function SentimentAnalysis() {
   };
 
   return (
-    <NewDesignSideBar
-      toggleSidebar={toggleSidebar}
-      needSearchMobile="needSearchMobile"
-      closeSidebar={() => setToggleSidebar(!toggleSidebar)}
-    >
-      <div className={styles.page__wrap}>
-        <TopNav
-          openSidebar={() => {
-            setToggleSidebar(!toggleSidebar);
-          }}
-        />
-        <div className={styles.page__container}>
-          <div className={styles.audio__mobile}>
-            <AudioCard sentimentData={sentimentData} />
-          </div>
-          <div className={styles.sentiment__tab__opener}>
-            <div className={styles.opener__content} onClick={openSentimentTab}>
-              Overall sentiment
-              <div className={styles.arrow__container}>
-                <img src={blueArrowIcon} alt="arrow icon" />
-              </div>
-            </div>
-            <SentimentAside
-              isMobileAsideOpen={isMobileAsideOpen}
-              closeFunction={closeSentimentTab}
-              sentimentData={sentimentData}
-            />
-          </div>
-          <main className={styles.main__container}>
-            <span className={styles.main__container__top}>
-              <div
-                className={styles.main__container__top__arrow__icon__container}
-              >
-                <Link to={`/transcriptions/${params.AudioId}`}>
-                  <img
-                    className={
-                      styles.main__container__top__arrow__icon__container__image
-                    }
-                    src={arrowIcon}
-                    alt="arrow icon"
-                  />
-                </Link>
-              </div>
-              <div className={styles.main__container__top__title}>
-                Sentiment analysis
-              </div>
-            </span>
-            <div className={styles.analysis__cards}>
-              <AnalysisCard sentimentData={sentimentData} />;
-            </div>
-          </main>
-          <aside className={styles.aside__container}>
-            <AudioCard sentimentData={sentimentData} />
-            <OverAllSentimentCard sentimentData={sentimentData} />
-            <VerdictCard sentimentData={sentimentData} />
-            <PhraseTagCard
-              tags={
-                sentimentData.positiveTags
-                  ? sentimentData.positiveTags
-                  : positiveTags
-              }
-              title={"Positive phrase tags"}
-              sentimentData={sentimentData}
-            />
-            <PhraseTagCard
-              tags={
-                sentimentData.negativeTags
-                  ? sentimentData.negativeTags
-                  : negativeTags
-              }
-              title={"Negative phrase tags"}
-              sentimentData={sentimentData}
-            />
-          </aside>
-        </div>
+    <div className={styles.page__container}>
+      <div className={styles.audio__mobile}>
+        <AudioCard sentimentData={sentimentData} />
       </div>
-    </NewDesignSideBar>
+      <div className={styles.sentiment__tab__opener}>
+        <div className={styles.opener__content} onClick={openSentimentTab}>
+          Overall sentiment
+          <div className={styles.arrow__container}>
+            <img src={blueArrowIcon} alt="arrow icon" />
+          </div>
+        </div>
+        <SentimentAside
+          isMobileAsideOpen={isMobileAsideOpen}
+          closeFunction={closeSentimentTab}
+          sentimentData={sentimentData}
+        />
+      </div>
+      <main className={styles.main__container}>
+        <span className={styles.main__container__top}>
+          <div className={styles.main__container__top__arrow__icon__container}>
+            <Link to={`/transcriptions/${params.AudioId}`}>
+              <img
+                className={
+                  styles.main__container__top__arrow__icon__container__image
+                }
+                src={arrowIcon}
+                alt="arrow icon"
+              />
+            </Link>
+          </div>
+          <div className={styles.main__container__top__title}>
+            Sentiment analysis
+          </div>
+        </span>
+        <div className={styles.analysis__cards}>
+          <AnalysisCard sentimentData={sentimentData} />;
+        </div>
+      </main>
+      <aside className={styles.aside__container}>
+        <AudioCard sentimentData={sentimentData} />
+        <OverAllSentimentCard sentimentData={sentimentData} />
+        <VerdictCard sentimentData={sentimentData} />
+        <PhraseTagCard
+          tags={
+            sentimentData.positiveTags
+              ? sentimentData.positiveTags
+              : positiveTags
+          }
+          title={"Positive phrase tags"}
+          sentimentData={sentimentData}
+        />
+        <PhraseTagCard
+          tags={
+            sentimentData.negativeTags
+              ? sentimentData.negativeTags
+              : negativeTags
+          }
+          title={"Negative phrase tags"}
+          sentimentData={sentimentData}
+        />
+      </aside>
+    </div>
   );
 }
 
