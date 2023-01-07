2f018f237051d619f23401476a188521fb3aef0c
Tamunonengiyeofori <tkennwariso@gmail.com>
2023-01-07 21:30:14 +0100

    feat/BAC-17: created endpoint to upgrade and downgrade user plans

--- backend/app/routers/plan.py
+++ backend/app/routers/plan.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 import auth
 import crud
 import math
-
+from fastapi.responses import RedirectResponse
 
 plan_router = APIRouter(
     prefix="/plans",
@@ -17,14 +17,15 @@ def convert_plan(plan_data: dict, user:models.User, db: Session):
     company = db.query(models.Company).filter(models.Company.id == user.company_id).first()
     old_plan = plan_data["current_plan"]
     balance_time = plan_data["minutes"]
-    if old_plan != "free":
-        initial_plan = crud.get_plan_by_name(db, old_plan)
+    if old_plan != "Free":
+        initial_plan = db.query(models.ProductPlan).filter(models.ProductPlan.name == old_plan).first()
+        print(initial_plan)
         initial_plan_price = initial_plan.price
         upgrade_plan = crud.get_plan_by_name(db, plan_data["new_plan"])
         upgrade_plan_price = upgrade_plan.price
         
         cash_amount = balance_time * initial_plan_price
-        upload_time = float(cash_amount / upgrade_plan_price)
+        upload_time = int(cash_amount / upgrade_plan_price)
         
         if upload_time < 3600:
             raise HTTPException(status_code=status.HTTP_402_PAYMENT_REQUIRED, detail="Your balance is too low for this plan")
@@ -36,6 +37,10 @@ def convert_plan(plan_data: dict, user:models.User, db: Session):
             db.commit()
             db.refresh(company)
             return {"message": f"plan succesfully changed to {plan_data['new_plan']}"}
+    else: 
+        # raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You need to Subscribe for a plan")
+        return RedirectResponse(url="https://heed.cx/pricing", status_code=status.HTTP_303_SEE_OTHER)
+        return {"message": "You need to subscribe for a plan"}
 
 
 
@@ -45,5 +50,6 @@ def change_plan(user_plan:schema.ChangePlan , logged_in_user:models.User = Depen
                     "new_plan": user_plan.plan,
                     "minutes": logged_in_user.company.time_left}
     return convert_plan(plan_details, logged_in_user, db)
+
         
 
