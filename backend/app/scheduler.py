2d694fbb841c1e145923cd0199440de24b4c5ee1
David Akwuru <daakwuru@gmail.com>
2023-01-08 23:48:07 +0000

    Update scheduler.py

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -11,12 +11,14 @@ cron_schedule = Rocketry(config={"task_execution": "async"})
 
 @cron_schedule.task(every("3 seconds"))
 async def another():
-    await cron_status.check_and_update_jobs()  
-
-
-@cron_schedule.task(cron("* * * * *"), execution="main") 
-async def constantly():
+    #await cron_status.check_and_update_jobs()
     await cron_status.transcription_mail()
+
+
+
+#@cron_schedule.task(cron("* * * * *"), execution="main") 
+#async def constantly():
+#    await cron_status.transcription_mail()
 
 @cron_schedule.task(daily)
 async def account_deletion_reminder():
