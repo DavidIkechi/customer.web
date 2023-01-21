52e6d0e5c635f5480719d6dcccdfeae510483447
dprincecoder <dprincecoder@gmail.com>
2023-01-21 14:21:29 +0100

    payment card done

--- scrybe_frontend/src/pages/Pricing/CheckoutPage/index.jsx
+++ scrybe_frontend/src/pages/Pricing/CheckoutPage/index.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Check from "../assets/check.svg";
+import selectArr from "../assets/select-arrow.svg";
 import { PricingData } from "../Plans/data";
 import styles from "./checkout.module.scss";
 const CheckoutPage = () => {
   const selectedPlanKey = localStorage.getItem("selectedPlan");
   const [selectedPlan, setSelectedPlan] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [totalPay, setTotalPay] = useState(0);
 
-  // get selected plan from all plans
   const getSelectedPlan = (plankey) => {
     if (selectedPlanKey) {
       let thePlan = PricingData.filter((plan) => plan.planKey === plankey)[0];
       setSelectedPlan(thePlan);
     }
+  };
+
+  const getMin = (e) => {
+    const min = e.target.value;
+    const toPay = min * selectedPlan.pricing;
+    setTotalPay(toPay);
   };
 
   console.log("selectedPlan", selectedPlan);
@@ -29,28 +37,79 @@ const CheckoutPage = () => {
           <div className={styles.billingDropDown}>
             <h3>choose a different plan:</h3>
             <p>Click dropdown to switch between available plans</p>
-            <div className={styles.billingDropDown__input}></div>
-            <div className={styles.billingDropDown__modal}>
-              <div className={styles.modalContent}>
-                {PricingData.map((plan, index) => (
-                  <div className={styles.modalContent__item} key={index}>
-                    <div className={styles.nameDetails}>
-                      <p>{plan.headDescription}</p>
-                      <p>{plan.title}</p>
-                      <p>{plan.duration}</p>
-                    </div>
-                    <div className={styles.priceDetails}>
-                      <div className={styles.priceDetails__price}>
-                        <p>${plan.pricing}</p>
-                        <p>Per minute</p>
-                      </div>
-                      <div className={styles.priceDetails__button}>
-                        click to select
-                      </div>
-                    </div>
+            <div className={styles.billingDropDown__input}>
+              {selectedPlan && (
+                <div className={styles.inputItem}>
+                  <div className={styles.nameDetails}>
+                    <p>{selectedPlan.headDescription} Plan</p>
+                    <h2>{selectedPlan.title}</h2>
+                    <p>{selectedPlan.duration}</p>
                   </div>
-                ))}
+                  <div className={styles.priceDetails}>
+                    <div className={styles.priceDetails__price}>
+                      <h2>${selectedPlan.pricing}</h2>
+                      <p>Per minute</p>
+                    </div>
+                    <img
+                      onClick={() => setIsModalOpen(!isModalOpen)}
+                      src={selectArr}
+                      className={`${styles.arrow} ${
+                        isModalOpen && styles.open
+                      }`}
+                      alt="select arrow"
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
+            {isModalOpen && (
+              <div className={styles.billingDropDown__modal}>
+                <div className={styles.modalContent}>
+                  {PricingData.map((plan, index) => (
+                    <div
+                      className={`${styles.modalContent__item} ${
+                        plan.headDescription === "Enterprise Plus" &&
+                        styles.plus
+                      }`}
+                      key={index}
+                    >
+                      <div className={styles.nameDetails}>
+                        <p>{plan.headDescription}</p>
+                        <h2>{plan.title}</h2>
+                        <p>{plan.duration}</p>
+                      </div>
+                      <div className={styles.priceDetails}>
+                        <div className={styles.priceDetails__price}>
+                          <h2>${plan.pricing}</h2>
+                          <p>Per minute</p>
+                        </div>
+                        <div className={styles.priceDetails__button}>
+                          click to select
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
+            )}
+          </div>
+          <div className={styles.paymentCard}>
+            <h3>Minutes</h3>
+            <p>Type in the amount of minutes you want to buy</p>
+            <div className={styles.paymentCard__input}>
+              <div className={styles.paymentCardInputBox}>
+                <input type="number" placeholder="200" onChange={getMin} />
+                <p>minutes</p>
+              </div>
+              <p>X</p>
+              <div className={styles.planPrice}>
+                ${selectedPlan && selectedPlan.pricing}
+              </div>
+            </div>
+            <div className={styles.hr} />
+            <div className={styles.paymentCard__total}>
+              <h3>Total to pay</h3>
+              <h1 className={styles.paymentPrice}>${totalPay}</h1>
             </div>
           </div>
         </div>
